6af00f99b42778180aca2a3df5ca72cb389bc0ef
Djayden-R <6953@rgomiddelharnis.nl>
2026-09-11 20:47:48 +0200

    update Dockerfile and run.sh

--- recorder/run.sh
+++ recorder/run.sh
@@ -1,22 +1,21 @@
-#!/with-contenv bashio
+#!/bin/sh
 set -e
 
 CONFIG_PATH=/data/options.json
-RECORDER_CONF=/config/recorder.conf
 
 # Get app configuration
 OTR_HOST=$(jq --raw-output '.otr_host' $CONFIG_PATH)
 OTR_PORT=$(jq --raw-output '.otr_port' $CONFIG_PATH)
 OTR_USER=$(jq --raw-output '.otr_user' $CONFIG_PATH)
 OTR_PASS=$(jq --raw-output '.otr_pass' $CONFIG_PATH)
+export OTR_USER OTR_PASS
 
-# Write the configuration to recorder.conf
-cat << EOF > $RECORDER_CONF
-OTR_HOST="${OTR_HOST}"
-OTR_PORT=${OTR_PORT}
-OTR_USER="${OTR_USER}"
-OTR_PASS="${OTR_PASS}"
-EOF
+TOPIC=${OTR_TOPICS:-$OTR_TOPIC}
+TOPIC=${TOPIC:-'owntracks/#'}
 
-# Start owntracks recorder with the configuration
-exec /usr/sbin/ot-recorder --config $RECORDER_CONF
+[ -f "${OTR_STORAGEDIR:-/store}/ghash/data.mdb" ] || /usr/sbin/ot-recorder --initialize
+
+exec /usr/sbin/ot-recorder \
+	--host "$OTR_HOST" \
+	--port "$OTR_PORT" \
+	"$TOPIC"
