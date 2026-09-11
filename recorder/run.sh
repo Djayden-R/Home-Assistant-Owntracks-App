0d0b6d7e5b798b6b755dcb7a728256e28c36c8d0
Djayden-R <6953@rgomiddelharnis.nl>
2026-09-11 20:51:56 +0200

    update sh and version

--- recorder/run.sh
+++ recorder/run.sh
@@ -3,11 +3,13 @@ set -e
 
 CONFIG_PATH=/data/options.json
 
-# Get app configuration
-OTR_HOST=$(jq --raw-output '.otr_host' $CONFIG_PATH)
-OTR_PORT=$(jq --raw-output '.otr_port' $CONFIG_PATH)
-OTR_USER=$(jq --raw-output '.otr_user' $CONFIG_PATH)
-OTR_PASS=$(jq --raw-output '.otr_pass' $CONFIG_PATH)
+[ -f "$CONFIG_PATH" ] && OTR_HOST=$(jq --raw-output '.otr_host // empty' "$CONFIG_PATH")
+[ -f "$CONFIG_PATH" ] && OTR_PORT=$(jq --raw-output '.otr_port // empty' "$CONFIG_PATH")
+[ -f "$CONFIG_PATH" ] && OTR_USER=$(jq --raw-output '.otr_user // empty' "$CONFIG_PATH")
+[ -f "$CONFIG_PATH" ] && OTR_PASS=$(jq --raw-output '.otr_pass // empty' "$CONFIG_PATH")
+
+OTR_HOST=${OTR_HOST:-127.0.0.1}
+OTR_PORT=${OTR_PORT:-1883}
 export OTR_USER OTR_PASS
 
 TOPIC=${OTR_TOPICS:-$OTR_TOPIC}
